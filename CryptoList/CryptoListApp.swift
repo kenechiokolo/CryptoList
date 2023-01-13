de2ec283f0ebc3c53f2bc4a94226cb73a573e2c4
KC Okolo <mr@kenech.io>
2023-01-13 14:15:19 +0000

    Initial Commit

---
+++ CryptoList/CryptoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoListApp.swift
+//  CryptoList
+//
+//  Created by KC Okolo on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
